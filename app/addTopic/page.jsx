58462fb098c041d50413c4db9643c3a9839d8640
Kumar Deepam <kumar.deepam@gmail.com>
2023-09-10 03:11:32 +0530

    create addTopic page

---
+++ app/addTopic/page.jsx
@@ -0,0 +1,21 @@
+export default function addTopic() {
+  return (
+    <form className="flex flex-col gap-3">
+      <input
+        className="border border-slate-f00 px-8 py-2"
+        type="text"
+        placeholder="Enter title for this topic"
+      />
+
+      <input
+        className="border border-slate-f00 px-8 py-2"
+        type="text"
+        placeholder="Enter description for this topic"
+      />
+
+      <button className=" bg-green-600 font-bold text-white px-12 py-2 w-fit">
+        Save
+      </button>
+    </form>
+  );
+}
--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
       <body className="max-w-3xl mx-auto p-4">
         <div className={inter.className}>
           <Navbar />
-          {children}
+          <div className="mt-8">{children}</div>
         </div>
       </body>
     </html>
